60a0b9b0c7637b77d2959043cc75f70a2748182f
ahmed2193 <ahmed.alfawali@gmail.com>
2022-05-07 00:53:22 +0200

    first commit

---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String transportsHeader = 'transports';
+  static const String webSocketOption = 'websocket';
+  static const String pollingOption = 'polling';
+  static const String socketUrl =
+      " 10cf0e2642a6458da055b4400eb6b72e"; // Put your url as per server
+}
---
+++ lib/utils/constants/app_constants.dart
@@ -0,0 +1,6 @@
+// ignore: avoid_classes_with_only_static_members
+class AppConstants {
+  static const String mobileConnectionStatus = "ConnectivityResult.mobile";
+  static const String wifiConnectionStatus = "ConnectivityResult.wifi";
+  static const String agoraAppId = "10cf0e2642a6458da055b4400eb6b72e";
+}
